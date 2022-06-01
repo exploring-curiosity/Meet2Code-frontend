0b7f2e899f20021b18c8aa93351c6970f828f8ed
exploring-curiosity <curiositywake@gmail.com>
2022-06-01 18:54:06 +0530

    changed domain name

--- src/Meet2Code/config.js
+++ src/Meet2Code/config.js
@@ -1,4 +1,4 @@
 let serverEndpoint='https://meet2code.herokuapp.com'
-let clientEndpoint='https://main--tiny-seahorse-3d5e77.netlify.app'
+let clientEndpoint='https://meet2code.netlify.app'
 // let serverEndpoint='http://localhost:9000';
 export {serverEndpoint, clientEndpoint};
